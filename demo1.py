c0a618bf0707b019d9282751f107d1599652952b
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2021-06-17 17:04:13 +0530

    Sidebar Addition

--- demo1.py
+++ demo1.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd 
+from matplotlib import pyplot as plt
+from plotly import graph_objs as go
+from sklearn.linear_model import LinearRegression
+import numpy as np 
 
-st.title("Hello Streamlit")
+
+st.title("COVID-19 Detector")
+
+nav = st.sidebar.radio("Navigation",["Testing","Contribute"])
