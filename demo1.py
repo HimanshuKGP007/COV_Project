a3dfeaa37bb14e6d3be720c4d07ed11f7ac7e799
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2021-06-17 17:19:56 +0530

    Ideas

--- demo1.py
+++ demo1.py
@@ -9,3 +9,13 @@ import numpy as np
 st.title("COVID-19 Detector")
 
 nav = st.sidebar.radio("Navigation",["Testing","Contribute"])
+# Trying to add Audio Record feature
+if nav == "Contribute":
+    st.header("Contribute to our dataset")
+    ex = st.number_input("Enter your Name",0.0,20.0) # Name_imput
+    sal = st.number_input("Enter your Salary",0.00,1000000.00,step = 1000.0)# Covid Positive or healthy -- Dropdown box
+    if st.button("submit"):
+        to_add = {"Name":[ex],"Salary":[sal]}
+        to_add = pd.DataFrame(to_add)
+        to_add.to_csv("data//Salary_Data.csv",mode='a',header = False,index= False)
+        st.success("Submitted")
