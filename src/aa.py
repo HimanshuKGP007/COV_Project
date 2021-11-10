6188e12f00180df8a8e2ec18dfa232249b4899c6
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2021-11-10 12:28:44 +0530

    BTP Presentation + App images

---
+++ src/aa.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 10:35:04 2021
+
+@author: DELL
+"""
+
+from sklearn.datasets import load_iris
+
+# Load Iris data (https://en.wikipedia.org/wiki/Iris_flower_data_set)
+iris = load_iris()
